e5ab05ba091bde61cba74799b46113cca03a396f
Felix Nana Yaw Yeboah - Jefferson <87368189+felixyeboah@users.noreply.github.com>
2024-04-01 14:11:43 +0000

    added some functions to create and get all coffees

---
+++ controllers/coffee.go
@@ -0,0 +1,41 @@
+package controllers
+
+import (
+	"coffee-app/helpers"
+	"coffee-app/services"
+	"net/http"
+)
+
+var coffeeService services.Coffee
+
+// GetCoffees GET /coffees
+func GetCoffees(w http.ResponseWriter, r *http.Request) {
+	coffees, err := coffeeService.GetAll()
+	if err != nil {
+		helpers.MessageLogs.ErrorLog.Println(err)
+		helpers.WriteJson(w, http.StatusInternalServerError, helpers.Envelop{"error": "Internal Server Error"})
+		return
+	}
+
+	helpers.WriteJson(w, http.StatusOK, helpers.Envelop{"data": coffees})
+}
+
+// CreateCoffee POST /coffees
+func CreateCoffee(w http.ResponseWriter, r *http.Request) {
+	coffee := services.Coffee{}
+	err := helpers.ReadJson(w, r, &coffee)
+	if err != nil {
+		helpers.MessageLogs.ErrorLog.Println(err)
+		helpers.WriteJson(w, http.StatusBadRequest, helpers.Envelop{"error": "Invalid request payload"})
+		return
+	}
+
+	createdCoffee, err := coffeeService.Create(coffee)
+	if err != nil {
+		helpers.MessageLogs.ErrorLog.Println(err)
+		helpers.WriteJson(w, http.StatusInternalServerError, helpers.Envelop{"error": "Internal Server Error"})
+		return
+	}
+
+	helpers.WriteJson(w, http.StatusCreated, helpers.Envelop{"data": createdCoffee})
+}
